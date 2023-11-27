b9f7c131ca10bf4af6f908b0238719f2a0e7bae6
Sean <seankim912@yahoo.com>
2023-11-26 22:18:30 -0600

    logic for collision detection

--- DragDrop.cs
+++ DragDrop.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class DragDrop : MonoBehaviour
 {
     private bool isDragging = false;
+    private bool isOverDropZone = false;
+    private GameObject dropZone;
     private Vector2 startPosition;
 
     // Update is called once per frame
@@ -17,8 +19,30 @@ public class DragDrop : MonoBehaviour
     }
 
     /*
-        Since this script is a child of the card asset itself, you won't have a GameObject to drag and bind the script to in the Unity window.
-        Go to Add Component and add the Event Triggers -> Begin Drag and End Drag.
+    Remember to have DropZone higher than PlayerArea in Hierarchy to prevent bugs.
+    When dealing with collisions you have to manage Collision Layers. Add layer for Cards and set
+    card assets to that layer. Then in Project Settings > Physics 2D > Layer Collision Matrix tab,
+    uncheck "Cards" to tell Unity that you don't want cards to collide with other cards.
+    */
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isOverDropZone = true;
+        dropZone = collision.gameObject;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isOverDropZone = false;
+        dropZone = null;
+    }
+
+    /*
+    Since this script is a child of the card asset itself, you won't have a GameObject to drag and bind the script to in the Unity window.
+    Go to Add Component and add the Event Triggers -> Begin Drag and End Drag.
+    For DropZone functionality, Add Component -> Box Collider 2D and Rigid Body 2D. Edit Collider to match dimensions of Card. In Rigid Body
+    set Drag and Gravity to 0, Collision Detection to Continuous, and check X, Y, Z for constraints. Be sure to add a corresponding Box
+    Collider to your DropZone.
     */
 
     public void StartDrag()
@@ -30,5 +54,13 @@ public class DragDrop : MonoBehaviour
     public void EndDrag()
     {
         isDragging = false;
+        if (isOverDropZone)
+        {
+            transform.SetParent(dropZone.transform, false);
+        }
+        else
+        {
+            transform.position = startPosition;
+        }
     }
 }
