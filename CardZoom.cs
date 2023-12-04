de87486280eaac7d6e69e49044766440f8ac2adf
Sean <seankim912@yahoo.com>
2023-12-03 21:26:10 -0600

    card zoom functionality complete

--- CardZoom.cs
+++ CardZoom.cs
@@ -14,16 +14,35 @@ public class CardZoom : MonoBehaviour
     }
 
     /*
-    Add event handlers Pointer Enter and Pointer Exit in Card prefab, and tie to relevant ZoomCard.PointerEnter/Exit
+    Drag script onto prefab inspector, add event handlers Pointer Enter and Pointer Exit in Card prefab, and tie to relevant ZoomCard.PointerEnter/Exit
     script and handler in the dropdown menu.
     */
     public void OnHoverEnter()
     {
-        zoomCard = Instantiate(gameObject, new Vector2(Input.mousePosition.x, Input.mousePosition.y), Quaternion.identity);
+        zoomCard = Instantiate(gameObject, new Vector2(Input.mousePosition.x, Input.mousePosition.y + 250), Quaternion.identity);
+        zoomCard.transform.SetParent(Canvas.transform, false); // Here you will set the zoomed card to be instantiated within the canvas (the parent)
+        zoomCard.layer = LayerMask.NameToLayer("Zoom"); // Sets layer to Zoom so it won't collide, which you should create in Inspector
+        /*
+        Make sure to uncheck Zoom x UI and Cards in Project Settings > Layer Collision Matrix so they don't mess with each other
+
+        To make cardZoom go away once you start dragging the card, add another method to Begin Drag for Card1 and call the
+        CardZoom.OnHoverExit so it destroys the cardZoom
+        */
+
+        RectTransform rect = zoomCard.GetComponent<RectTransform>(); // You can already see this RectTransform component in the inspector before you even grabbed it here
+        rect.sizeDelta = new Vector2(240, 344);
+
+        /*
+        The zoomCard will flash on and off because essentially it's being recreated for every collision and the game doesn't know
+        if it's hovering over the original card or the zoomCard. Adding 250 units to the Instantiate y position above, for example,
+        causes zoomCard to instantiate in a place physically away from the original so the game doesn't get confused. Will have to
+        tailor code specifically depending on situation.
+         */
     }
 
     public void OnHoverExit()
     {
-
+        Destroy(zoomCard);
     }
 }
+
